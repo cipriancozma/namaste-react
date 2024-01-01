b6495f96663eb9bcea08abfbbc36b5dbb4758f55
Ciprian <cozma_ciprian19@yahoo.com>
2024-01-01 23:43:06 +0200

    formated readme file

---
+++ components/RestaurantCard.jsx
@@ -0,0 +1,13 @@
+const RestaurantCard = () => {
+  return (
+    <div className="restaurant-card">
+      <img alt="restaurant-logo" className="restaurant-logo" />
+      <h3>KFC</h3>
+      <h4>Biryani, Indian Cuisine</h4>
+      <h4>4.4 stars</h4>
+      <h4>40 minutes</h4>
+    </div>
+  );
+};
+
+export default RestaurantCard;
